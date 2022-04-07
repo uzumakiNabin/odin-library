525d46b10ef1825e6bc4b911988f0033b7a42405
uzumakiNabin <nabin.kaucha@gmail.com>
2022-04-07 23:04:23 +0545

    adding and deleting book works

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const addBookFormToggleBtn = document.getElementById('addBookFormToggleBtn');
+const form = document.getElementsByClassName('formContainer')[0];
+const closeForm = document.getElementById('close-mark');
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
 const bookPages = document.getElementById('pages');
 const hasReadBook = document.getElementsByName('hasRead');
+const addBookForm = document.getElementsByTagName('form')[0];
 const addBookBtn = document.getElementById('addBookBtn');
 const showField = document.getElementById('show');
 
@@ -16,7 +19,7 @@ function book(title, author, pages, hasRead){
 };
 
 function addBookToLibrary(e){
-    //e.preventDefault();
+    e.preventDefault();
     let hasReadInput;
     hasReadBook.forEach(radioButton => {
         if(radioButton.checked){
@@ -32,47 +35,80 @@ function addBookToLibrary(e){
         )
     );
     showBooksFromLibrary();
-    document.getElementById('formContainer').style.display = 'none';
+    form.classList.add('hidden');
 }
 
 function showBooksFromLibrary(){
     showField.innerHTML = "";
-    myLibrary.forEach(book => {
+    myLibrary.forEach((book, index) => {
         let card = document.createElement('div');
         card.classList.add('card');
+
         let cardTitle = document.createElement('h3');
-        cardTitle.classList.add('cardTitle');
+        cardTitle.classList.add('card-title');
         cardTitle.textContent = book.title;
+
         let cardAuthor = document.createElement('p');
-        cardAuthor.classList.add('cardAuthor');
+        cardAuthor.classList.add('card-author');
         cardAuthor.textContent = book.author;
+
         let cardPages = document.createElement('p');
-        cardPages.classList.add('cardPages');
+        cardPages.classList.add('card-pages');
         cardPages.textContent = `${book.pages} pages`;
-        let cardReadStatus = document.createElement('p');
-        cardReadStatus.classList.add('cardReadStatus');
-        cardReadStatus.textContent = "Mark this book as read.";
-        let cardReadStatusCheck = document.createElement('input');
-        cardReadStatusCheck.type = 'checkbox';
+
+        let readStatusBtn = document.createElement('input');
+        readStatusBtn.type = 'button';
+        readStatusBtn.value = 'Mark as read';
+        readStatusBtn.classList.add('btn', 'readBtn');
         if(book.hasRead === 'read'){
-            cardReadStatusCheck.checked = true;
+            readStatusBtn.classList.add('bookCompleted');
+            readStatusBtn.value = '✔ Completed';
         }
-        else{
-            cardReadStatusCheck.checked = false;
-        }
+        readStatusBtn.addEventListener('click', () => {
+            if(book.hasRead === 'read'){
+                book.hasRead = 'notread';
+            }
+            else{
+                book.hasRead = 'read';
+            }
+            showBooksFromLibrary();
+        });
+
+        let editBtn = document.createElement('input');
+        editBtn.type = 'button';
+        editBtn.value = 'Edit';
+        editBtn.classList.add('btn', 'editBtn');
+        editBtn.addEventListener('click', () => {
+            document.getElementById('formContainer').style.display = 'block';
+        });
+
+        let deleteBtn = document.createElement('input');
+        deleteBtn.type = 'button';
+        deleteBtn.value = "Remove";
+        deleteBtn.classList.add('btn', 'deleteBtn');
+        deleteBtn.addEventListener('click', () => {
+            myLibrary.splice(index, 1);
+            showBooksFromLibrary();
+        });
+
         card.appendChild(cardTitle);
         card.appendChild(cardAuthor);
         card.appendChild(cardPages);
-        card.appendChild(cardReadStatus);
-        card.appendChild(cardReadStatusCheck);
+        card.appendChild(readStatusBtn);
+        card.appendChild(editBtn);
+        card.appendChild(deleteBtn);
         showField.append(card);
     })
 }
 
 addBookFormToggleBtn.addEventListener('click', () => {
-    document.getElementById('formContainer').style.display = 'block';
+    form.classList.remove('hidden');
 });
 
-addBookBtn.addEventListener('click', addBookToLibrary);
+addBookForm.addEventListener('submit', addBookToLibrary);
+
+closeForm.addEventListener('click', () => {
+    form.classList.add('hidden');
+})
 
 showBooksFromLibrary();
